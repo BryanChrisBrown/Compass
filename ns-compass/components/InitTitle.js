c5f40380a09bfd68e19ba80295c19213a095c2c9
Bryan Chris Brown <>
2020-09-20 20:50:50 -0400

    removed airbnb eslint, adjusted organization of title screen, added loading progress bar + auto navigation via next/useRouter.
    animation timings still need to be adjusted, perhaps route shouldn't be called until the circle is done animating. spacing of elements in the title sequence need to be updated to match the figma design.

--- ns-compass/components/InitTitle.js
+++ ns-compass/components/InitTitle.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React from "react";
 // import PropTypes from 'prop-types';
-import Link from 'next/link';
+import Link from "next/link";
+import { useRouter } from 'next/router'
+
+// class LoadingProgress extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { loading: true }
+//   }
+
+//   render() {
+//     const loading = this.state.loading
+
+//     return (
+//       <div
+//         onAnimationEnd={() => Link(href="/home")}
+//         className={'loadingProgress'}>
+//       </div>
+//     )
+//   }
+// }
+
+function LoadingProgress() { 
+const router = useRouter()
+return (
+  <div
+    className={'loadingProgress'}
+    onAnimationEnd={() => router.push('/home')}
+    >
+  </div>
+)}
+
 
 class Title extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       doneLoading: false,
-      loadingString: 'Beaming up Scotty',
+      loadingString: "Beaming up Scotty",
     };
   }
 
@@ -19,41 +49,38 @@ class Title extends React.Component {
     // in the future, we can set up an Eel function with a callback
     // that will load out devices and use async/await with said callback to
     // set our state and continue
-    async function simLoading (thisParam) {
+    async function simLoading(thisParam) {
       const self = thisParam;
       await setTimeout(() => {
-        console.log('setstate');
+        console.log("setstate");
         self.setState({ doneLoading: true });
         self.props.loaderCallback(true);
-      },
-      4000);
+      }, 4000);
     }
     simLoading(this);
   }
 
   render() {
     return (
-    // eslint-disable-next-line no-unused-vars
-      <div role="button" className="ns-title">
-        <h1>Compass</h1>
-        <div className="ns-line" />
-        <p>Powered by Community</p>
-        <div>
-          {this.state.doneLoading
-              && (
-              <Link href="/home">
-                <a>go to app page</a>
-              </Link>
-              )}
-          {!this.state.doneLoading
-                  && (
-                    <p className="loader-msg">
-                        {this.state.loadingString}
-                      <span>.</span>
-                      <span>.</span>
-                      <span>.</span>
-                    </p>
-                  )}
+      // eslint-disable-next-line no-unused-vars
+      <div className="fullscreen">
+        <div role="button" className="ns-title">
+          <h1>Compass</h1>
+          <div className="ns-line" />
+          <div style={{ top: -40 }}>
+            <p>Powered by Community</p>
+          </div>
+          <div>
+          <div className="loadingBackground"><LoadingProgress/></div>
+            {!this.state.doneLoading && (
+              <p className="loader-msg">
+                {this.state.loadingString}
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+              </p>
+            )}
+          </div>
         </div>
       </div>
     );
