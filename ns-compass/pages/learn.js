3034bde2e1713fd1e19e3d94405f1b473b2c707a
Mojo <47996003+1MaddieX@users.noreply.github.com>
2020-10-08 11:56:31 -0400

    WIP learn page

--- ns-compass/pages/learn.js
+++ ns-compass/pages/learn.js
@@ -7,6 +7,7 @@ import NSButton from "../components/Button";
 import Bgrnd from "../components/InitBg";
 import NavBar from "../components/NavBar";
 import NSMenu from "../components/Menu";
+import Blob from "../components/Blob";
 import { useRouter } from "next/router";
 export default class AppHome extends React.Component {
   // WARN: must use window.eel to keep parse-able eel.expose{...}
@@ -19,7 +20,48 @@ export default class AppHome extends React.Component {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <NavBar />
-        <main className={styles.main}>page coming soon</main>
+        <main>
+          <div className="ns-container">
+            <div
+              style={
+                {
+                  /*height: "300px", 
+                "font-size": "0.9 em", */
+                }
+              }
+              className="row ns-flex justify-between"
+            >
+              <div className="ns-flex justify-between">
+                <NSMenu menuID={"1"}>
+                  <p active="leapmotion">Hardware</p>
+                  <p active="connection">2D Calibration</p>
+                  <p active="bandwidth">3D Calibration</p>
+                  <p active="tracking">Camera Calibration</p>
+                </NSMenu>
+              </div>
+            </div>
+
+            <div style={{ height: "40px" }} />
+
+            <div
+              style={
+                {
+                  /*height: "300px"*/
+                }
+              }
+              className="row ns-flex justify-between"
+            >
+              <div className="ns-flex justify-between">
+                <NSMenu menuID={"2"}>
+                  <p active="realsense">Software</p>
+                  <p active="connection">Optics</p>
+                  <p active="bandwidth">Tracking</p>
+                  <p active="tracking">Meshing</p>
+                </NSMenu>
+              </div>
+            </div>
+          </div>
+        </main>
         <Bgrnd className="blurred" />
       </div>
     );
